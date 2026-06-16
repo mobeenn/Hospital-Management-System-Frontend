dcf5f74cf40f8c2a71e2e1a0f9c0531cd0264b25
Mobeen <Mobeen.infini8ai@gmail.com>
2026-06-16 05:26:04 +0500

    add backend url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+// const api = axios.create({
+//    baseURL: "http://localhost:5000/api",
+// });
 const api = axios.create({
-   baseURL: "http://localhost:5000/api",
+   baseURL: "https://hospital-management-system-frontend-dusky.vercel.app/api",
 });
-
 api.interceptors.request.use((config) => {
    const token = localStorage.getItem("token");
 
